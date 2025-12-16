bcb9e8008a6eeaba23a39e8083d225c38e27b947
Diogo <fmlsp2015@gmail.com>
2025-12-16 05:57:30 -0300

    chore[linting]: add eslintrc file to root

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+  ],
+};
+
