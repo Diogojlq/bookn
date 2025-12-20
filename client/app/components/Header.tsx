e6b9f522dbf218617bd210f9f50273fcfdfe7ad0
Diogo <fmlsp2015@gmail.com>
2025-12-20 15:54:27 -0300

    feat: add header items list

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,7 +1,16 @@
+import { NavigationMenuList } from "@radix-ui/react-navigation-menu";
 import { NavigationMenu } from "../../components/ui/navigation-menu";
+import { NavigationMenuTrigger } from "../../components/ui/navigation-menu";
+import { NavigationMenuItem } from "../../components/ui/navigation-menu";
 
 export default function Header() {
   return (
-    <NavigationMenu />
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </ NavigationMenu>
   )
 }
