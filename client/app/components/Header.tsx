54028ef4e21530fc54ade375f5caf7604aef8954
Diogo <fmlsp2015@gmail.com>
2025-12-20 15:09:26 -0300

    feat[client]: create Header component

---
+++ client/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <ul>
+      <li><h6 >Dropdown header</h6></li>
+      <li><a >Action</a></li>
+      <li><a href="#">Another action</a></li>
+    </ul>
+  )
+}
