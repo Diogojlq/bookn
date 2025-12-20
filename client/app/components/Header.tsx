b2b31ac263eca3e0e4b513ca385d632bba791b57
Diogo <fmlsp2015@gmail.com>
2025-12-20 15:29:47 -0300

    feat: import NavigationMenu from shadcn

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,9 +1,7 @@
+import { NavigationMenu } from "../../components/ui/navigation-menu";
+
 export default function Header() {
   return (
-    <ul>
-      <li><h6 >Dropdown header</h6></li>
-      <li><a >Action</a></li>
-      <li><a href="#">Another action</a></li>
-    </ul>
+    <NavigationMenu />
   )
 }
