9035d1a48cb5d62ba38e88b0013ac779a4fa3776
Diogo <fmlsp2015@gmail.com>
2025-12-24 12:33:17 -0300

    feat: add links to header

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,16 +1,34 @@
 import { NavigationMenuList } from "../../components/ui/navigation-menu.tsx";
 import { NavigationMenu } from "../../components/ui/navigation-menu";
-import { NavigationMenuTrigger } from "../../components/ui/navigation-menu";
+import { NavigationMenuLink } from "../../components/ui/navigation-menu";
 import { NavigationMenuItem } from "../../components/ui/navigation-menu";
 
 export default function Header() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
+    <NavigationMenu className="max-w-full w-full justify-center">
+      <NavigationMenuList className="flex gap-2">
+
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Home</NavigationMenuTrigger>
+          <NavigationMenuLink href="/">
+            <NavigationMenuLink>
+              Home
+            </NavigationMenuLink>
+          </NavigationMenuLink>
         </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink href="/about">
+            About
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            Register
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
       </NavigationMenuList>
-    </ NavigationMenu>
+    </NavigationMenu>
   )
 }
