00384a92e81dd031da627478e2272a7d7f5a0b2f
Diogo <fmlsp2015@gmail.com>
2025-12-27 14:55:34 -0300

    feat: implement basic router for bookings

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+const router = Router();
 
-export default Router().get("/hello", (_req, res) =>
-  res.json({ message: "Hello World!" })
-);
+router.post('bookings', (req, res) => {
+  const { date, doctor } = req.body;
+  res.status(201).send({ message: "Booking confirmed!" })
+});
+
+export default router;
