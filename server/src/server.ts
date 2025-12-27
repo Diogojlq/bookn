02b253f989e5df25f655adae24597e6169d0613d
Diogo <fmlsp2015@gmail.com>
2025-12-27 14:45:48 -0300

    refactor: delete server init from server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,4 @@ import express, { type Express } from "express";
 
 const app: Express = express();
 
-const PORT = 4444
-
-app.get("/", (_req, res) => {
-  res.send("Hello World!")
-})
-
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`)
-})
+export { app };
