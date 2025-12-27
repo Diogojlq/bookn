6a4c0524eba4af8cf4080279c5c0d1baeb2bc0af
Diogo <fmlsp2015@gmail.com>
2025-12-27 15:03:26 -0300

    reafctor: add slash to url

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-router.post('bookings', (req, res) => {
+router.post('/bookings', (req, res) => {
   const { date, doctor } = req.body;
   res.status(201).send({ message: "Booking confirmed!" })
 });
