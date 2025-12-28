f284bbbfa19d0871f9882747c8a1c2518a6c775c
Diogo <fmlsp2015@gmail.com>
2025-12-28 14:17:04 -0300

    feat: user register endpoint

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -6,4 +6,9 @@ router.post('/bookings', (req, res) => {
   res.status(201).send({ message: "Booking confirmed!" })
 });
 
+router.post('/user', (req, res) => {
+  const { name, email } = req.body;
+  res.status(201).send({ message: "User created!" })
+});
+
 export default router;
