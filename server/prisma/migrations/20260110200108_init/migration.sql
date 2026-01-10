eba9814bade30a8f07d37ee316606e7a19a3a6c2
Diogo <fmlsp2015@gmail.com>
2026-01-10 17:02:48 -0300

    feat: initial database migration

---
+++ server/prisma/migrations/20260110200108_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "password" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Slot" (
+    "id" TEXT NOT NULL,
+    "date" DATE NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT,
+    "isAvailable" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Slot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Slot" ADD CONSTRAINT "Slot_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
