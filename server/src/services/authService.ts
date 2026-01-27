d8e729c1614d2b4cb61b69e5b753081d857f3b1a
Diogo <fmlsp2015@gmail.com>
2026-01-27 19:03:42 -0300

    feat: add authService class and register funtion

---
+++ server/src/services/authService.ts
@@ -0,0 +1,25 @@
+import prisma from '../../prisma/prisma';
+import bcrypt from 'bcrypt';
+
+export class authService {
+  async register(email: string, name: string, password: string) {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new Error("Email already used")
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prsima.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    });
+    return { id: user.id, email: user.email, name: user.name };
+  }
+}
