066e87f80f6b1032135a444a23cc3bf8da750ec1
Diogo <fmlsp2015@gmail.com>
2026-02-16 17:48:27 -0300

    feat: better Hero responsiveness

--- client/app/components/Hero.tsx
+++ client/app/components/Hero.tsx
@@ -2,11 +2,11 @@ import { LoginCard } from "./LoginCard";
 
 export default function Hero() {
   return (
-    <div className="grid grid-cols-2 gap-4 p-8">
-      <div className="flex items-center justify-center bg-gray-100 rounded-lg p-8">
-        <h1 className="text-2xl font-bold">Left Column</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8">
+      <div className="flex items-center justify-center rounded-lg p-8">
+        <h1 className="text-4xl font-bold">The better way to keep track of your appointments</h1>
       </div>
-      <div className="flex items-center justify-center bg-gray-200 rounded-lg p-8">
+      <div className="flex items-center justify-center rounded-lg p-8">
         <LoginCard />
       </div>
     </div>
