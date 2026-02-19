d067cee78aaf6f6f54fa878ff05068f84050d9d8
Diogo <fmlsp2015@gmail.com>
2026-02-19 17:05:36 -0300

    chore: add CORS configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express, { type Express } from "express";
 import router from './routes/router';
+import cors from "cors";
 
 const app: Express = express();
 app.use(express.json())
 
+app.use(cors({
+  origin: "http://localhost:3001",
+  credentials: true
+}));
 app.use(router)
 
 export { app };
